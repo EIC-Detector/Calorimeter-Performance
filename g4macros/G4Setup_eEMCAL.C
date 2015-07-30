09e0b966f6bec5f2d2f32829112fbc40d6d49626
jlab <joshua.labounty@stonybrook.edu>
2015-07-30 15:38:25 -0400

    Updates Fun4All_G4_eEMCAL_ZeroField.C to utilize the most recent version of the Crystal Calorimeter

--- g4macros/G4Setup_eEMCAL.C
+++ g4macros/G4Setup_eEMCAL.C
@@ -1,17 +1,14 @@
 using namespace std;
 
 // global macro parameters
-double Min_forward_eta = 1.15;
+//double Min_forward_eta = 1.15;
 
-double no_overlapp = 0.0001; // added to radii to avoid overlapping volumes
 bool overlapcheck = false; // set to true if you want to check for overlaps
 
 void
 G4Init()
 {
   // load detector macros
-  gROOT->LoadMacro("G4Detector_Cone_eEMCAL.C");
-  eEMCAL_Init();
 }
 
 /* simplified setup including only limited subdetectors
@@ -38,25 +35,14 @@ G4Setup(const int absorberactive = 0, const float field = 0)
   /////////////////////////////////////////////////
   //  electron going detectors
   /////////////////////////////////////////////////
-  //double newzpos = G4Detector_Cone_eEMCAL(g4Reco, -99., -1.2, -4.5, 18. ); // 18cm thick PbWO4 crystal calorimeter
 
   PHG4CrystalCalorimeterSubsystem *eecal = new PHG4CrystalCalorimeterSubsystem("eEMCAL");
   eecal->OverlapCheck(overlapcheck);
   g4Reco->registerSubsystem( eecal );
 
-  /**
-   * 'spy' tracking layer infront of eEMCAL to capture particle positions right before they
-   * enter the caorimeter
-   */
-  //  PHG4ConeSubsystem *spy_eecal = new PHG4ConeSubsystem("SPYEEMCAL", 0);
-  //  spy_eecal->SetZlength(0.0001);
-  //  spy_eecal->SetPlaceZ(-98.9);
-  //  spy_eecal->Set_eta_range(-1.2, -4.5);
-  //  spy_eecal->SetMaterial("G4_AIR");
-  //  spy_eecal->SetActive(true);
-  //  spy_eecal->SuperDetector("SPYEEMCAL");
-  //  spy_eecal->OverlapCheck(overlapcheck);
-  //  g4Reco->registerSubsystem(spy_eecal);
+  PHG4EnvelopeSubsystem *envelope = new PHG4EnvelopeSubsystem("ENVELOPE");
+  envelope->OverlapCheck(overlapcheck);
+  g4Reco->registerSubsystem( envelope );
 
   PHG4TruthSubsystem *truth = new PHG4TruthSubsystem();
   g4Reco->registerSubsystem(truth);
