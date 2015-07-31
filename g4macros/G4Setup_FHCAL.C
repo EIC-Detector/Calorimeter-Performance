dc12e46e96824542be3781d961587656fb8f79d4
Nils Feege <nils.feege@stonybrook.edu>
2015-07-31 07:04:35 -0400

    Add Fun4All macro to run forward HCAL stand-alone

---
+++ g4macros/G4Setup_FHCAL.C
@@ -0,0 +1,57 @@
+using namespace std;
+
+// global macro parameters
+double Min_forward_eta = 1.15;
+
+double no_overlapp = 0.0001; // added to radii to avoid overlapping volumes
+bool overlapcheck = false; // set to true if you want to check for overlaps
+
+void
+G4Init()
+{
+}
+
+/* simplified setup including only limited subdetectors
+ */
+void
+G4Setup(const int absorberactive = 0, const float field = 0)
+{
+  //---------------
+  // Load libraries
+  //---------------
+
+  gSystem->Load("libg4detectors.so");
+
+  //---------------
+  // Fun4All server
+  //---------------
+  Fun4AllServer *se = Fun4AllServer::instance();
+
+  PHG4Reco* g4Reco = new PHG4Reco();
+  g4Reco->set_rapidity_coverage(1.1); // temperary value assigned by ePHENIX LoI
+
+  g4Reco->set_field(field); // use const solenoid field
+
+  PHG4ForwardHcalSubsystem *hhcal = new PHG4ForwardHcalSubsystem("FHCAL");
+  hhcal->OverlapCheck(overlapcheck);
+  g4Reco->registerSubsystem( hhcal );
+
+  /**
+   * 'spy' tracking layer infront of calorimeter to capture particle positions right before they
+   * enter the caorimeter
+   */
+  PHG4ConeSubsystem *spy_hhcal = new PHG4ConeSubsystem("SPYHHCAL", 0);
+  spy_hhcal->SetZlength(0.0001);
+  spy_hhcal->SetPlaceZ(349.9);
+  spy_hhcal->Set_eta_range(1.2, 5.0);
+  spy_hhcal->SetMaterial("G4_AIR");
+  spy_hhcal->SetActive(true);
+  spy_hhcal->SuperDetector("SPYHHCAL");
+  spy_hhcal->OverlapCheck(overlapcheck);
+  g4Reco->registerSubsystem(spy_hhcal);
+
+  PHG4TruthSubsystem *truth = new PHG4TruthSubsystem();
+  g4Reco->registerSubsystem(truth);
+
+  se->registerSubsystem(g4Reco);
+}
