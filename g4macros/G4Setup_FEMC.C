78500ef6391e8ccf7a050f8a45cd19bb8b1e06c4
Nils Feege <nils.feege@stonybrook.edu>
2015-10-10 13:36:48 -0400

    Update to new macro structure

--- g4macros/G4Setup_FEMC.C
+++ g4macros/G4Setup_FEMC.C
@@ -1,19 +1,17 @@
-#include "/direct/phenix+u/nfeege/sphenixsw/devel/install/g4cemc/include/g4cemc/CaloTowerID.h"
-
 using namespace std;
 
 // global macro parameters
 bool overlapcheck = false; // set to true if you want to check for overlaps
 
 void
-G4Init()
+FEMCInit()
 {
 }
 
 /* simplified setup including only limited subdetectors
  */
 void
-G4Setup(const int absorberactive = 0, const float field = 0)
+FEMCSetup(PHG4Reco* g4Reco, const int absorberactive = 0)
 {
   //---------------
   // Load libraries
@@ -26,24 +24,17 @@ G4Setup(const int absorberactive = 0, const float field = 0)
   //---------------
   Fun4AllServer *se = Fun4AllServer::instance();
 
-  PHG4Reco* g4Reco = new PHG4Reco();
-  g4Reco->set_rapidity_coverage(1.1); // temperary value assigned by ePHENIX LoI
-
-  g4Reco->set_field(field); // use const solenoid field
-
 
   /** Use dedicated FEMC module */
   PHG4ForwardEcalSubsystem *femc = new PHG4ForwardEcalSubsystem("FEMC");
 
   ostringstream mapping_femc;
 
-  /* path to local copy of calibrations repository */
-  //mapping_femc << "calibrations";
-
   /* path to central copy of calibrations repositry */
   mapping_femc << getenv("OFFLINE_MAIN") << "/share/calibrations";
 
   mapping_femc << "/ForwardEcal/mapping/towerMap_FEMC_v002.txt";
+
   cout << mapping_femc.str() << endl;
 
   femc->SetTowerMappingFile( mapping_femc.str() );
@@ -58,7 +49,7 @@ G4Setup(const int absorberactive = 0, const float field = 0)
   //G4_FEcal(g4Reco, 350.0, 1.1, 5.0, 100.);
 
   CaloTowerGeomManager* geoman = CaloTowerGeomManager::instance();
-  geoman->ReadGeometryFromTable( calotowerid::FEMC , mapping_femc.str() );
+  geoman->ReadGeometryFromTable( "FEMC" , mapping_femc.str() );
 
   /**
    * 'spy' tracking layer infront of calorimeter to capture particle positions right before they
@@ -74,8 +65,4 @@ G4Setup(const int absorberactive = 0, const float field = 0)
   spy_femc->OverlapCheck(overlapcheck);
   g4Reco->registerSubsystem(spy_femc);
 
-  PHG4TruthSubsystem *truth = new PHG4TruthSubsystem();
-  g4Reco->registerSubsystem(truth);
-
-  se->registerSubsystem(g4Reco);
 }
--- g4macros/G4Setup_FHCAL.C
+++ g4macros/G4Setup_FHCAL.C
@@ -1,19 +1,17 @@
-#include "/direct/phenix+u/nfeege/sphenixsw/devel/install/g4cemc/include/g4cemc/CaloTowerID.h"
-
 using namespace std;
 
 // global macro parameters
 bool overlapcheck = false; // set to true if you want to check for overlaps
 
 void
-G4Init()
+FHCALInit()
 {
 }
 
 /* simplified setup including only limited subdetectors
  */
 void
-G4Setup(const int absorberactive = 0, const float field = 0)
+FHCALSetup(PHG4Reco* g4Reco, const int absorberactive = 0)
 {
   //---------------
   // Load libraries
@@ -26,19 +24,11 @@ G4Setup(const int absorberactive = 0, const float field = 0)
   //---------------
   Fun4AllServer *se = Fun4AllServer::instance();
 
-  PHG4Reco* g4Reco = new PHG4Reco();
-  g4Reco->set_rapidity_coverage(1.1); // temperary value assigned by ePHENIX LoI
-
-  g4Reco->set_field(field); // use const solenoid field
-
 
   /** Use dedicated FHCAL module */
   PHG4ForwardHcalSubsystem *hhcal = new PHG4ForwardHcalSubsystem("FHCAL");
 
   ostringstream mapping_hhcal;
-
-  /* path to local copy of calibrations repository */
-  //mapping_hhcal << "calibrations";
 
   /* path to central copy of calibrations repositry */
   mapping_hhcal << getenv("OFFLINE_MAIN") << "/share/calibrations";
@@ -60,13 +50,8 @@ G4Setup(const int absorberactive = 0, const float field = 0)
 
   /* Read tower geometry on the fly
    */
-  /** Load mapping / geometry for analysis */
-  //CaloTowerGeomReader *georead = new CaloTowerGeomReader("CaloTowerGeoRead");
-  //georead->ReadGeometryFromTable( calotowerid::FHCAL , mapping_hhcal.str() );
-  //g4Reco->registerSubsystem( georead );
-
   CaloTowerGeomManager* geoman = CaloTowerGeomManager::instance();
-  geoman->ReadGeometryFromTable( calotowerid::FHCAL , mapping_hhcal.str() );
+  geoman->ReadGeometryFromTable( "FHCAL" , mapping_hhcal.str() );
 
   /**
    * 'spy' tracking layer infront of calorimeter to capture particle positions right before they
@@ -82,8 +67,4 @@ G4Setup(const int absorberactive = 0, const float field = 0)
   spy_hhcal->OverlapCheck(overlapcheck);
   g4Reco->registerSubsystem(spy_hhcal);
 
-  PHG4TruthSubsystem *truth = new PHG4TruthSubsystem();
-  g4Reco->registerSubsystem(truth);
-
-  se->registerSubsystem(g4Reco);
 }
